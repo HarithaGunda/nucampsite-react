338578a009368dd8d2f9b3d4664b1df3d18c778e
Haritha Gunda <gunda.haritha@gmail.com>
2019-12-30 13:30:53 -0500

    Components Part 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'typeface-lobster';
+import 'typeface-open-sans';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
