491f822eabe499bbe22fd7a9b991d1438446bbbd
Haritha Gunda <gunda.haritha@gmail.com>
2020-01-06 17:32:49 -0500

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-import { CAMPSITES } from './shared/campsites';
-import Directory from './components/DirectoryComponent';
+import Main from './components/MainComponent';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state={campsites:CAMPSITES};
-    }
-  render() {
+     render() {
       return (
           <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={this.state.campsites}/>
+              <Main/>
           </div>
       );
   }
--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ export class CampsiteInfo extends Component{
             return (
                 <span className="col-md-5 m-1">
                     <h4>Comments</h4>
-                   {comments.map(comment => <div>{comment.text}
+                   {comments.map(comment => <div key={comment.id}>{comment.text}
                                                  <br></br>
                                                  {'--'.concat((comment.author).concat(','))}
                                                  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
@@ -37,9 +37,10 @@ export class CampsiteInfo extends Component{
     render(){
         //if(!(this.props.campsite==null) && !(typeof this.props.campsite === 'undefined'))
         if(this.props.campsite)
-            return <div className="row">{this.renderCampsite(this.props.campsite)}
+            return  <div className="container">
+                    <div className="row">{this.renderCampsite(this.props.campsite)}
                             {this.renderComments(this.props.campsite.comments)}
-            </div>;
+                            </div></div>;
         return <div/>;
     }
 }
