10ce5ea037e3c9dd262a076f0f92834a4b68f83c
Haritha Gunda <gunda.haritha@gmail.com>
2020-01-10 17:17:48 -0500

    Footer and Header Added along with converting the CamsiteInfoComp,DirectoryComp in to functional comp

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,11 +1,10 @@
-import React,{Component} from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
-export class CampsiteInfo extends Component{
-    renderCampsite(campsite){
+    function RenderCampsite({campsite}) {
         return (
             <div className="col-md-5 m-1">
-                 <Card>
+                <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
                         <CardTitle>{campsite.name}</CardTitle>
@@ -15,32 +14,36 @@ export class CampsiteInfo extends Component{
             </div>
         );
     }
-    renderComments(comments){
-        if (comments)
-        {
+    function RenderComments({comments}) {
+        if (comments) {
             return (
                 <span className="col-md-5 m-1">
                     <h4>Comments</h4>
-                   {comments.map(comment => <div key={comment.id}>{comment.text}
-                                                 <br></br>
-                                                 {'--'.concat((comment.author).concat(','))}
-                                                 {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                                 <br></br>
-                                                 <br></br>
-                                            </div>) 
-                    }   
+                    {comments.map(comment => <div key={comment.id}>{comment.text}
+                        <br></br>
+                        {'--'.concat((comment.author).concat(','))}
+                        {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        <br></br>
+                        <br></br>
+                    </div>)
+                    }
                 </span>
             );
         }
-        return <div/>;
+        return <div />;
     }
-    render(){
+    function CampsiteInfo(props) {
         //if(!(this.props.campsite==null) && !(typeof this.props.campsite === 'undefined'))
-        if(this.props.campsite)
-            return  <div className="container">
-                    <div className="row">{this.renderCampsite(this.props.campsite)}
-                            {this.renderComments(this.props.campsite.comments)}
-                            </div></div>;
-        return <div/>;
+        if (props.campsite){
+            return (
+                <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite}/>
+                    <RenderComments comments={props.campsite.comments}/>
+                </div>
+                </div>
+                );
+        }
+        return <div />;
     }
-}
+export default CampsiteInfo;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
