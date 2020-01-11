16e707fafaf2c6446109eb9257fd9fb0a2478a11
Haritha Gunda <gunda.haritha@gmail.com>
2020-01-10 21:53:46 -0500

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import CampsiteInfo from "./CampsiteInfoComponent";
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -30,12 +31,21 @@ class Main extends Component {
         );
     }
 
+    const CampsiteWithId= ({match}) => {
+        return (
+            <CampsiteInfo 
+                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+            />
+        );
+    };    
       return (
           <div>
               <Header />
               <Switch>
                   <Route path='/home' component={HomePage}/>
                   <Route exact path='/directory' render={()=>{ return <Directory campsites={this.state.campsites}/>}}/>
+                  <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                   <Route exact path='/contactus' component={Contact}/>
                   <Redirect to='/home'/>
               </Switch>
