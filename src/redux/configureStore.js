8c82fea848989294e7c710f5fd4453b2d79976e7
Haritha Gunda <gunda.haritha@gmail.com>
2020-01-19 21:07:43 -0500

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,12 @@
-import {createStore} from 'redux';
-import {Reducer,initialState} from './reducer';
+import {createStore,combineReducers} from 'redux';
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+
+
 export const ConfigureStore=()=>
 {
-    const store=createStore(Reducer,initialState);
+    const store=createStore(combineReducers({campsites:Campsites,comments:Comments,partners:Partners,promotions:Promotions}));
     return store;
 }
